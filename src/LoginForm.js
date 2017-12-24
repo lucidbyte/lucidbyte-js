1f39f13e86c34b473ea2a407e387940c2fdfa32f
Leland Kwong <leland.kwong@gmail.com>
2017-12-23 22:52:36 -0800

    add LoginForm component and refactor some of the auth code

---
+++ src/LoginForm.js
@@ -0,0 +1,118 @@
+import { h, render, Component } from 'preact';
+
+// Tell Babel to transform JSX into h() calls:
+/** @jsx h */
+
+const EmailPrompt = ({ onSubmit, onChange, value }) => (
+  <form onSubmit={onSubmit}>
+    <p><strong>Please enter your email</strong></p>
+    <input
+      className='input'
+      type='email'
+      autoComplete='email'
+      placeholder='your_email@domain.com'
+      value={value}
+      onChange={onChange}
+    />
+  </form>
+);
+
+const AwaitingConfirmation = ({ onSubmit, onChange, value }) => (
+  <div>
+    <p><strong>A one-time code has been sent to your email.</strong></p>
+    <form onSubmit={onSubmit}>
+      <input
+        className='input'
+        type='text'
+        placeholder='your code'
+        value={value}
+        onChange={onChange}
+      />
+    </form>
+  </div>
+);
+
+export default class LoginForm extends Component {
+  state = {
+    email: '',
+    emailSent: false,
+
+    code: ''
+  }
+
+  componentDidMount() {
+    const { projectID } = this.props;
+    const {
+      auth: {
+        getAccessToken,
+        login,
+      }
+    } = require('./auth').default({ projectID });
+    this.login = login;
+    this.getAccessToken = getAccessToken;
+  }
+
+  handleSubmit = (ev) => {
+    ev.preventDefault();
+    const { email } = this.state;
+    this.login(email).then(res => {
+      console.log(res);
+      this.setState({ emailSent: true });
+    }).catch(err => {
+      console.error(err);
+    });
+  }
+
+  handleEmailChange = (ev) => {
+    const value = ev.target.value;
+    this.setState({ email: value });
+  }
+
+  handleCodeSubmit = (ev) => {
+    ev.preventDefault();
+    const loginCode = this.state.code;
+    this.getAccessToken(loginCode)
+      .then(() => {
+        location.assign('/account');
+      });
+  }
+
+  handleCodeChange = (ev) => {
+    const value = ev.target.value;
+    this.setState({ code: value });
+  }
+
+  render() {
+    return (
+      <div>
+        {!this.state.emailSent
+          ? (
+            <EmailPrompt
+              onChange={this.handleEmailChange}
+              onSubmit={this.handleSubmit}
+              value={this.state.email}
+            />
+          ) : (
+            <AwaitingConfirmation
+              onChange={this.handleCodeChange}
+              onSubmit={this.handleCodeSubmit}
+              value={this.state.code}
+            />
+          )
+        }
+      </div>
+    );
+  }
+}
+
+LoginForm.render = ({
+  projectID,
+  element
+}) => {
+  render(
+    <LoginForm
+      projectID={projectID}
+    />,
+    element
+  );
+};
---
+++ src/auth.js
@@ -0,0 +1,166 @@
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const handlePromiseJSON = res => res.data;
+
+const handleJSONdata = json => {
+  if (json.errors) {
+    console.error('graphqlError: \n' + JSON.stringify(json.errors, null, 2));
+  }
+  return json.data;
+};
+
+const session = (
+  method, sessionParams = {}
+) => {
+  const {
+    accessToken,
+    expiresAt
+  } = sessionParams;
+  return {
+    accessToken: localStorage[`${method}Item`]('accessToken', accessToken),
+    expiresAt: localStorage[`${method}Item`]('expiresAt', expiresAt)
+  };
+};
+
+const getSession = () => session('get');
+const endSession = () => session('remove');
+const setSession = (sessionParams) => session('set', sessionParams);
+const hasSession = () => !!session('get').accessToken;
+
+const constructRequestHeaders = async () => {
+  const accessToken = getSession().accessToken;
+  const headers = {
+    // Authorization: `Bearer ${accessToken}`,
+    'Content-Type': 'application/json'
+  };
+  if (accessToken) {
+    headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return headers;
+};
+
+const constructApiUrl = (apiRoute = '', projectID, customOrigin) => {
+  const apiOrigin = customOrigin
+    || (
+      isDev
+        ? 'http://localhost:3000'
+        : 'https://test.lelandkwong.com'
+    );
+  const endpoint = `${apiOrigin}/graphql`;
+
+  const apiUrl = apiRoute ? `${apiOrigin}${apiRoute}` : `${endpoint}/${projectID}`;
+  return apiUrl;
+};
+
+export default function AuthInstance({
+  origin: customOrigin,
+  // noted-api config
+  projectID
+}) {
+  if (typeof document === 'undefined') {
+    return {};
+  }
+
+  const client = async (query, variables, url) => {
+    return axios({
+      url: constructApiUrl(url, projectID, customOrigin),
+      method: 'POST',
+      headers: await constructRequestHeaders(),
+      data: {
+        query,
+        variables
+      },
+    }).then(handlePromiseJSON)
+      .then(handleJSONdata);
+  };
+
+  const isLoggedIn = () => {
+    return hasSession();
+  };
+
+  const callbacks = [];
+  const authStateChangeFn = () => {
+    callbacks.forEach(cb => cb({ loggedIn: hasSession() }));
+  };
+  const onAuthStateChange = (callback) => {
+    callbacks.push(callback);
+    authStateChangeFn();
+  };
+
+  const logout = () => {
+    endSession();
+    authStateChangeFn();
+  };
+
+  const login = async (email, projectID) => {
+    return axios({
+      url: constructApiUrl(`/api/login`, projectID, customOrigin),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        email,
+        projectID
+      }
+    });
+  };
+
+  const getAccessToken = (loginCode) => {
+    return fetch(`/api/access-token/${loginCode}`, {
+      method: 'GET',
+    }).then(res => res.json())
+      .then(json => {
+        console.log(json);
+        setSession(json);
+        authStateChangeFn();
+        return json;
+      });
+  };
+
+  const getRefreshToken = async () => {
+    return axios({
+      url: constructApiUrl(`/api/refresh-token`, projectID, customOrigin),
+      headers: await constructRequestHeaders(),
+      method: 'GET'
+    }).then(res => {
+      const { accessToken, expiresAt } = res.data;
+      setSession({ accessToken, expiresAt });
+      return accessToken;
+    });
+  };
+
+  let hasExpired = false;
+
+  const scheduleTokenRefresh = () => {
+    const { expiresAt } = getSession();
+    const padding = 1000 * 60 * 60;
+    const delay =  expiresAt - new Date().getTime() - padding;
+    hasExpired = delay <= 0;
+    if (!hasExpired) {
+      setTimeout(() => {
+        getRefreshToken()
+          .then(scheduleTokenRefresh);
+      }, delay);
+    } else {
+      logout();
+    }
+  };
+
+  if (isLoggedIn()) {
+    scheduleTokenRefresh();
+  }
+
+  return {
+    client,
+    auth: {
+      getAccessToken,
+      login,
+      logout,
+      onAuthStateChange,
+    }
+  };
+}
